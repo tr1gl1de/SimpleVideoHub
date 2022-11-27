3c265d6fdead5dd61280cf09e637b4babe16ef42
Max <pogatop@yandex.ru>
2022-11-28 00:48:54 +0300

    Add method for create video
    * Added exception

---
+++ Application/Common/Exceptions/EntityIsExistsException.cs
@@ -0,0 +1,9 @@
+﻿namespace Application.Common.Exceptions;
+
+public class EntityIsExistsException : Exception
+{
+    public EntityIsExistsException(string name, string key)
+        : base($"Entity \"{name}\" ({key}) is exists.")
+    {
+    }
+}
